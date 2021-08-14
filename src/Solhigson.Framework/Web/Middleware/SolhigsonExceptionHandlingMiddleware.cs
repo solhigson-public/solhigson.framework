2544c0223753be3d21758caa4801c8d052ee6723
Emeka Awagu <eawagu@gmail.com>
2021-08-14 13:41:04 +0100

    framework - added exception handling middleware

---
+++ src/Solhigson.Framework/Web/Middleware/SolhigsonExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Solhigson.Framework.Infrastructure;
+using Solhigson.Framework.Utilities;
+
+namespace Solhigson.Framework.Web.Middleware
+{
+    public class SolhigsonExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                this.ELogError(e);
+                await HandleExceptionAsync(context);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsync(ResponseInfo.FailedResult("Internal Server Error").SerializeToJson());
+        }
+    }
+}
