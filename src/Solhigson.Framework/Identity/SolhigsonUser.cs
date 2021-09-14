790be9fbc9604f96e55ec5f67b1c30fa2228bbe3
Emeka Awagu <eawagu@gmail.com>
2021-09-14 12:06:38 +0100

    framework - added user

---
+++ src/Solhigson.Framework/Identity/SolhigsonUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Solhigson.Framework.Identity
+{
+    public class SolhigsonUser : IdentityUser
+    {
+        public bool IsEnabled { get; set; }
+        public bool RequirePasswordChange { get; set; }
+    }
+}
