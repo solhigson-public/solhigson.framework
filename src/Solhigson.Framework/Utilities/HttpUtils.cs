66514d89837e3a817f64bb69a5354e832e8e446f
Emeka Awagu <eawagu@gmail.com>
2021-09-20 13:19:23 +0100

    http utils

---
+++ src/Solhigson.Framework/Utilities/HttpUtils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Solhigson.Framework.Utilities
+{
+    public static class HttpUtils
+    {
+        public static string UrlRoot(HttpContext httpContext)
+        {
+            if (httpContext == null)
+            {
+                return string.Empty;
+            }
+
+            var scheme = httpContext.Request.Scheme;
+            if (httpContext.Request.IsHttps && !scheme.Contains("s"))
+            {
+                scheme = "https";
+            }
+
+            return $"{scheme}://{httpContext.Request.Host}";
+        }
+    }
+}
