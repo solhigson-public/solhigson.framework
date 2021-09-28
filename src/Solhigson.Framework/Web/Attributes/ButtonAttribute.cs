feb5f566e781f9bccbfedd265576e41586d01416
Emeka Awagu <eawagu@gmail.com>
2021-09-28 09:23:42 +0100

    button action selector attribute

---
+++ src/Solhigson.Framework/Web/Attributes/ButtonAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+
+namespace Solhigson.Framework.Web.Attributes
+{
+    public class ButtonAttribute : ActionMethodSelectorAttribute
+    {
+        public string ButtonName { get; set; }
+
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            return routeContext.HttpContext.Request.Form.ContainsKey(ButtonName);
+        }
+
+        public ButtonAttribute(string name)
+        {
+            ButtonName = name;
+        }
+    }
+}
