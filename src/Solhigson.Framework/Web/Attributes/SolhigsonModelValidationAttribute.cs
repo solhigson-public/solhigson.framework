ed0cfda4f82bbe7ba2016d6355b70da9617132e3
Emeka Awagu <eawagu@gmail.com>
2021-10-25 14:59:29 +0100

    capture model validation errors via attribute

---
+++ src/Solhigson.Framework/Web/Attributes/SolhigsonModelValidationAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Solhigson.Framework.Extensions;
+using Solhigson.Framework.Infrastructure;
+
+namespace Solhigson.Framework.Web.Attributes
+{
+    public class SolhigsonModelValidationAttribute : Attribute, IActionFilter, IOrderedFilter
+    {
+        public int Order { get; } = int.MaxValue - 99;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.Controller is not ControllerBase cont)
+            {
+                return;
+            }
+
+            if (cont.ModelState.IsValid)
+            {
+                return;
+            }
+            
+            if (!context.HttpContext.IsApiController())
+            {
+                return;
+            }
+
+            var error = new StringBuilder();
+            foreach (var modelError in cont.ModelState.Values.SelectMany(model => model.Errors))
+            {
+                error.AppendLine(modelError.ErrorMessage);
+            }
+
+            context.Result = new JsonResult(ResponseInfo.FailedResult(error.ToString()))
+            {
+                StatusCode = StatusCodes.Status200OK
+            };
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
--- src/Solhigson.Framework/Web/SolhigsonApiControllerBase.cs
+++ src/Solhigson.Framework/Web/SolhigsonApiControllerBase.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Solhigson.Framework.Infrastructure;
+using Solhigson.Framework.Web.Attributes;
 
 namespace Solhigson.Framework.Web
 {
     [ApiController]
+    [SolhigsonModelValidation]
     public class SolhigsonApiControllerBase : ControllerBase
     {
         
