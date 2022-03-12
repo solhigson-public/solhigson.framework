fbdff3ccd95531e6f159d86e1d65a0e28ff19be1
Emeka Awagu <eawagu@gmail.com>
2022-03-12 15:37:44 +0100

    updates

--- src/Solhigson.Framework/Dto/SolhigsonPermissionDto.cs
+++ src/Solhigson.Framework/Dto/SolhigsonPermissionDto.cs
@@ -4,7 +4,8 @@ namespace Solhigson.Framework.Dto;
 
 public record SolhigsonPermissionDto
 {
-
+    public string Name { get; set; }
+    
     public string Description { get; set; }
 
     public string Url { get; set; }
