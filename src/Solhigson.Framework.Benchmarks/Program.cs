7fafd04211c7dea59301b59b6bf480951b7b4091
Emeka Awagu <eawagu@gmail.com>
2022-03-16 20:49:05 +0100

    build fix

---
+++ src/Solhigson.Framework.Benchmarks/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Solhigson.Framework.Benchmarks.Whiteboard;
+
+IServiceProvider BuildDi(IConfiguration config)
+{
+    return new ServiceCollection()
+        //Add DI Classes here
+        .AddLogging(loggingBuilder =>
+        {
+            // configure Logging with NLog
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddNLog(config);
+        })
+        .AddSingleton<Benchmark>()
+        .BuildServiceProvider();
+}
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+    .Build();
+
+var provider = BuildDi(config);
+provider.GetRequiredService<Benchmark>().Execute();
+BenchmarkRunner.Run(typeof(Program).Assembly);
+
