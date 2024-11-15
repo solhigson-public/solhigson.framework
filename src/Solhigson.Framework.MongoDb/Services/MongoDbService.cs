e6ccd5f1bc75b9f9041d4b9863f27b6b1ace36b2
Emeka <>
2024-11-15 10:58:21 +0100

    updates

--- src/Solhigson.Framework.MongoDb/Services/MongoDbService.cs
+++ src/Solhigson.Framework.MongoDb/Services/MongoDbService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using NLog.Common;
@@ -62,7 +63,7 @@ public class MongoDbService
     }
 
         
-    public async Task<ResponseInfo<T>> AddDocumentAsync<T>(T document) where T : IMongoDbDocumentBase
+    public async Task<ResponseInfo<T>> AddDocumentAsync<T>(T document, CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var resp = new ResponseInfo<T>();
         var coll = GetCollection<T>();
@@ -70,31 +71,30 @@ public class MongoDbService
         {
             return resp.Fail();
         }
-        await coll.InsertOneAsync(document);
+        await coll.InsertOneAsync(document, cancellationToken: cancellationToken);
         return resp.Success(document);
     }
 
-    public async Task<T?> FindAsync<T>(string id) where T : IMongoDbDocumentBase
+    public async Task<T?> FindAsync<T>(string id, CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var coll = GetCollection<T>();
-        if (coll is null)
-        {
-            return default;
-        }
-        return await coll.Find(doc => doc.Id == id).SingleOrDefaultAsync();
+        return coll is null 
+            ? default 
+            : await coll.Find(doc => doc.Id == id).SingleOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<List<T>> FindAsync<T>(Expression<Func<T, bool>> filter) where T : IMongoDbDocumentBase
+    public async Task<List<T>> FindAsync<T>(Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var coll = GetCollection<T>();
         if (coll is null)
         {
             return [];
         }
-        return await coll.Find(filter).ToListAsync();
+        return await coll.Find(filter).ToListAsync(cancellationToken);
     }
 
-    public async Task<PagedList<T>> FindAsync<T>(Expression<Func<T, bool>> filter, int pageNumber, int pageSize) where T : IMongoDbDocumentBase
+    public async Task<PagedList<T>> FindAsync<T>(Expression<Func<T, bool>> filter, int pageNumber, int pageSize,
+        CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var coll = GetCollection<T>();
         if (coll is null)
@@ -102,29 +102,29 @@ public class MongoDbService
             return PagedList.Create<T>([], 0, 1, 1);
         }
 
-        var count = await coll.Find(filter).CountDocumentsAsync();
-        var result = await coll.Find(filter).Skip((pageNumber - 1) * pageSize).Limit(pageSize).ToListAsync();
+        var count = await coll.Find(filter).CountDocumentsAsync(cancellationToken);
+        var result = await coll.Find(filter).Skip((pageNumber - 1) * pageSize).Limit(pageSize).ToListAsync(cancellationToken);
         return PagedList.Create(result, count, pageNumber, pageSize);
     }
 
-    public async Task UpdateAsync<T>(T document) where T : IMongoDbDocumentBase
+    public async Task UpdateAsync<T>(T document, CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var coll = GetCollection<T>();
         if (coll is null)
         {
             return;
         }
-        await coll.ReplaceOneAsync(sub => sub.Id == document.Id, document);
+        await coll.ReplaceOneAsync(sub => sub.Id == document.Id, document, cancellationToken: cancellationToken);
     }
 
-    public async Task DeleteAsync<T>(string id) where T : IMongoDbDocumentBase
+    public async Task DeleteAsync<T>(string id, CancellationToken cancellationToken = default) where T : IMongoDbDocumentBase
     {
         var coll = GetCollection<T>();
         if (coll is null)
         {
             return;
         }
-        await coll.DeleteOneAsync(sub => sub.Id == id);
+        await coll.DeleteOneAsync(sub => sub.Id == id, cancellationToken: cancellationToken);
     }
 
     public static string GetCollectionName<T>() where T : IMongoDbDocumentBase
@@ -150,23 +150,25 @@ public class MongoDbService<T> where T : IMongoDbDocumentBase
         CollectionName = MongoDbService.GetCollectionName<T>();
     }
 
-    public async Task<ResponseInfo<T>> AddDocumentAsync(T document)
+    public async Task<ResponseInfo<T>> AddDocumentAsync(T document, CancellationToken cancellationToken = default)
     {
-        return await _mongoDbService.AddDocumentAsync(document);
+        return await _mongoDbService.AddDocumentAsync(document, cancellationToken);
     }
 
-    public async Task<T?> FindAsync(string id) => await _mongoDbService.FindAsync<T>(id);
+    public async Task<T?> FindAsync(string id, CancellationToken cancellationToken = default) => await _mongoDbService.FindAsync<T>(id, cancellationToken);
 
-    public async Task<List<T>> FindAsync(Expression<Func<T, bool>> filter) => await _mongoDbService.FindAsync(filter);
+    public async Task<List<T>> FindAsync(Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default) 
+        => await _mongoDbService.FindAsync(filter, cancellationToken);
     
-    public async Task<PagedList<T>> FindAsync(Expression<Func<T, bool>> filter, int pageNumber, int pageSize)
+    public async Task<PagedList<T>> FindAsync(Expression<Func<T, bool>> filter, int pageNumber, int pageSize,
+        CancellationToken cancellationToken = default)
     {
-        return await _mongoDbService.FindAsync(filter, pageNumber, pageSize);
+        return await _mongoDbService.FindAsync(filter, pageNumber, pageSize, cancellationToken);
     }
 
-    public async Task UpdateAsync(T document) => await _mongoDbService.UpdateAsync(document);
+    public async Task UpdateAsync(T document, CancellationToken cancellationToken = default) => await _mongoDbService.UpdateAsync(document, cancellationToken);
 
-    public async Task DeleteAsync(string id) => await _mongoDbService.DeleteAsync<T>(id);
+    public async Task DeleteAsync(string id, CancellationToken cancellationToken = default) => await _mongoDbService.DeleteAsync<T>(id, cancellationToken);
 }
 
 
