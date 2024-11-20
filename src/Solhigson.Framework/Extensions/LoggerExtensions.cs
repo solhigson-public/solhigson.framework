77b77d6cbf2e56248795f2c937e58923f6890459
Emeka <>
2024-11-20 11:47:58 +0100

    updates

--- src/Solhigson.Framework/Extensions/LoggerExtensions.cs
+++ src/Solhigson.Framework/Extensions/LoggerExtensions.cs
@@ -10,72 +10,72 @@ namespace Solhigson.Framework.Extensions;
 
 public static class LoggerExtensions
 {
-    private static void Log(object obj, LogLevel level, string? message, params object?[]? args)
+    private static void Log(object obj, LogLevel level, string? message, Exception? e = null, params object?[]? args)
     {
-        LogManager.GetLogger(obj).Log(level, message, args);
+        LogManager.GetLogger(obj).Log(level, message, e, args);
     }
     
     [Obsolete("This will be depreciated in future releases, use LogTrace() instead")]
     public static void ELogTrace(this object obj, string message, object? data = null)
     {
-        Log(obj, LogLevel.Trace, message, data);
+        Log(obj, LogLevel.Trace, message, null, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogTrace(this object obj, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Trace, message, args);
+        Log(obj, LogLevel.Trace, message, null, args);
     }
 
 
     [Obsolete("This will be depreciated in future releases, use LogDebug() instead")]
     public static void ELogDebug(this object obj, string message, object? data = null)
     {
-        Log(obj, LogLevel.Debug, message, data);
+        Log(obj, LogLevel.Debug, message, null, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogDebug(this object obj, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Debug, message, args);
+        Log(obj, LogLevel.Debug, message, null, args);
     }
 
 
     [Obsolete("This will be depreciated in future releases, use LogInfo() instead")]
     public static void ELogInfo(this object obj, string message, object? data = null)
     {
-        Log(obj, LogLevel.Information, message, data);
+        Log(obj, LogLevel.Information, message, null, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogInformation(this object obj, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Information, message, args);
+        Log(obj, LogLevel.Information, message, null, args);
     }
 
     [Obsolete("This will be depreciated in future releases, use LogWarning() instead")]
     public static void ELogWarn(this object obj, string message, object? data = null)
     {
-        Log(obj, LogLevel.Warning, message, data);
+        Log(obj, LogLevel.Warning, message, null, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogWarning(this object obj, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Warning, message, args);
+        Log(obj, LogLevel.Warning, message, null, args);
     }
 
 
     [Obsolete("This will be depreciated in future releases, use LogError() instead")]
     public static void ELogError(this object obj, string message, object? data = null)
     {
-        Log(obj, LogLevel.Error, message, data);
+        Log(obj, LogLevel.Error, message, null, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogError(this object obj, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Error, message, args);
+        Log(obj, LogLevel.Error, message, null, args);
     }
 
 
@@ -83,13 +83,13 @@ public static class LoggerExtensions
     public static void ELogError(this object obj, Exception e, string? message = null, object? data = null,
         string? userEmail = null)
     {
-        LogManager.GetLogger(obj).Log(LogLevel.Error, message, e, data);
+        Log(obj, LogLevel.Error, message, e, data);
     }
     
     [MessageTemplateFormatMethod("message")]
     public static void LogError(this object obj, Exception e, string? message = null, params object?[]? args)
     {
-        Log(obj, LogLevel.Error, message, args);
+        Log(obj, LogLevel.Error, message, e, args);
     }
 
 
@@ -97,13 +97,13 @@ public static class LoggerExtensions
     public static void ELogFatal(this object obj, string message, Exception? e = null, object? data = null,
         string? userEmail = null)
     {
-        LogManager.GetLogger(obj).Log(LogLevel.Critical, message, e, data);
+        Log(obj, LogLevel.Critical, message, e, data);
     }
     
     [MessageTemplateFormatMethod("message")]
-    public static void LogCritical(this object obj, string message, params object?[]? args)
+    public static void LogCritical(this object obj, Exception e, string message, params object?[]? args)
     {
-        Log(obj, LogLevel.Critical, message, args);
+        Log(obj, LogLevel.Critical, message, e, args);
     }
 
     public static ILogger? Logger(this object obj) => LogManager.GetLogger(obj).InternalLogger;
