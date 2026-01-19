9b8af9cf07908962088a756ed0f4162a4798b44f
Emeka Awagu <emeka.awagu@teamapt.com>
2026-01-19 10:40:53 +0100

    refactor: make `Description` and `AllowedRoles` properties in `PermissionInfoAttribute` immutable, increment `PackageVersion` to 10.0.8

--- src/Solhigson.Framework/Web/Attributes/PermissionInfoAttribute.cs
+++ src/Solhigson.Framework/Web/Attributes/PermissionInfoAttribute.cs
@@ -6,13 +6,13 @@ namespace Solhigson.Framework.Web.Attributes;
 public sealed class PermissionInfoAttribute(string name, string description) : Attribute
 {
     public string Name { get; } = name;
-    public string Description { get; set; } = description;
+    public string Description { get; } = description;
     public bool IsMenuRoot { get; set; }
     public bool IsMenu { get; set; }
     public int MenuIndex { get; set; }
     public string? Icon { get; set; }
     public string? ParentName { get; set; }
-    public string[]? AllowedRoles { get; set; }
+    public string[] AllowedRoles { get; set; } = [];
     internal string? Url { get; set; }
     public string? OnClickFunction { get; set; }
 }
